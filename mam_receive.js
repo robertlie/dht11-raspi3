3936370edd62e77f937def941dae5b361c61774a
Robert Lie <robertlie@users.noreply.github.com>
2018-07-16 08:56:37 +0200

    Replace tabs with spaces

--- mam_receive.js
+++ mam_receive.js
@@ -8,20 +8,20 @@ Instead of this file you can also use another tool to display the data:
 https://www.mobilefish.com/services/cryptocurrency/mam.html (Select option: Data receiver)
 
 Usage:
-1) 	You can change the default settings: MODE or SIDEKEY
-	If you do, make the same changes in mam_publish.js and mam_sensor.js files.
-2) 	Start the app: node mam_receive.js <root>
+1)  You can change the default settings: MODE or SIDEKEY
+    If you do, make the same changes in mam_publish.js and mam_sensor.js files.
+2)  Start the app: node mam_receive.js <root>
 
 More information:
 https://www.mobilefish.com/developer/iota/iota_quickguide_raspi_mam.html
 */
 
-const Mam 	= require('./lib/mam.client.js');
-const IOTA 	= require('iota.lib.js');
-const iota 	= new IOTA({ provider: 'https://nodes.testnet.iota.org:443' });
+const Mam = require('./lib/mam.client.js');
+const IOTA = require('iota.lib.js');
+const iota = new IOTA({ provider: 'https://nodes.testnet.iota.org:443' });
 
-const MODE 		= 'restricted'; 	// public, private or restricted
-const SIDEKEY 	= 'mysecret'; 		// Enter only ASCII characters. Used only in restricted mode
+const MODE = 'restricted'; // public, private or restricted
+const SIDEKEY = 'mysecret'; // Enter only ASCII characters. Used only in restricted mode
 
 let root;
 let key;
@@ -29,13 +29,13 @@ let key;
 // Check the arguments
 const args = process.argv;
 if(args.length !=3) {
-	console.log('Missing root as argument: node mam_receive.js <root>');
-	process.exit();
+    console.log('Missing root as argument: node mam_receive.js <root>');
+    process.exit();
 } else if(!iota.valid.isAddress(args[2])){
-	console.log('You have entered an invalid root: '+ args[2]);
-	process.exit();
+    console.log('You have entered an invalid root: '+ args[2]);
+    process.exit();
 } else {
-	root = args[2];
+    root = args[2];
 }
 
 // Initialise MAM State
@@ -43,20 +43,20 @@ let mamState = Mam.init(iota);
 
 // Set channel mode
 if (MODE == 'restricted') {
-	key = iota.utils.toTrytes(SIDEKEY);
-	mamState = Mam.changeMode(mamState, MODE, key);
+    key = iota.utils.toTrytes(SIDEKEY);
+    mamState = Mam.changeMode(mamState, MODE, key);
 } else {
-	mamState = Mam.changeMode(mamState, MODE);
+    mamState = Mam.changeMode(mamState, MODE);
 }
 
 // Receive data from the tangle
 const executeDataRetrieval = async function(rootVal, keyVal) {
-	let resp = await Mam.fetch(rootVal, MODE, keyVal, function(data) {
-		let json = JSON.parse(iota.utils.fromTrytes(data));
-		console.log(`dateTime: ${json.dateTime}, data: ${json.data}`);
-	});
+    let resp = await Mam.fetch(rootVal, MODE, keyVal, function(data) {
+        let json = JSON.parse(iota.utils.fromTrytes(data));
+        console.log(`dateTime: ${json.dateTime}, data: ${json.data}`);
+    });
 
-	executeDataRetrieval(resp.nextRoot, keyVal);
+    executeDataRetrieval(resp.nextRoot, keyVal);
 }
 
 executeDataRetrieval(root, key);
--- sensor.js
+++ sensor.js
@@ -5,34 +5,34 @@ The sensor.js file reads DHT11 sensor data (temperature and humidity) and displa
 This file will only work a Raspberry Pi.
 
 Usage:
-1) 	You can change the default setting: TIMEINTERVAL
-2)	Do not forget to type: npm install
-3)	Start the app: node sensor.js
-4) 	If you encounter problems reading the DHT11 sensor data and wants more logging:
-	Type: npm uninstall node-dht-sensor
-	Type: npm install node-dht-sensor --dht_verbose=true
+1)  You can change the default setting: TIMEINTERVAL
+2)  Do not forget to type: npm install
+3)  Start the app: node sensor.js
+4)  If you encounter problems reading the DHT11 sensor data and wants more logging:
+    Type: npm uninstall node-dht-sensor
+    Type: npm install node-dht-sensor --dht_verbose=true
 
-	If you have fixed your problem:
-	Type: npm uninstall node-dht-sensor
-	Type: npm install node-dht-sensor --dht_verbose=false
+    If you have fixed your problem:
+    Type: npm uninstall node-dht-sensor
+    Type: npm install node-dht-sensor --dht_verbose=false
 
 More information:
 https://www.mobilefish.com/developer/iota/iota_quickguide_raspi_mam.html
 */
 const sensor = require('node-dht-sensor');
 
-const TIMEINTERVAL 	= 10;  	// seconds
-const SENSORTYPE 	= 11;	// 11=DHT11, 22=DHT22
-const GPIOPIN 		= 4;	// The Raspi gpio pin where data from the DHT11 is read
+const TIMEINTERVAL = 10; // seconds
+const SENSORTYPE = 11; // 11=DHT11, 22=DHT22
+const GPIOPIN = 4; // The Raspi gpio pin where data from the DHT11 is read
 
 function readSensor(){
-	sensor.read(SENSORTYPE, GPIOPIN, function(err, temperature, humidity) {
-		if (!err) {
-			console.log('temp: ' + temperature.toFixed(1) + 'C, ' + 'humidity: ' + humidity.toFixed(1) + '%');
-		} else {
-			console.log(err);
-		}
-	});
+    sensor.read(SENSORTYPE, GPIOPIN, function(err, temperature, humidity) {
+        if (!err) {
+            console.log('temp: ' + temperature.toFixed(1) + 'C, ' + 'humidity: ' + humidity.toFixed(1) + '%');
+        } else {
+            console.log(err);
+        }
+    });
 }
 
 readSensor();
